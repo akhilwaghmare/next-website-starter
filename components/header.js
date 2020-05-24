be873357e2666a8e000b63e92e5a10e943b0ff5b
Akhil Waghmare <akhilwaghmare@gmail.com>
2020-05-24 12:17:55 -0400

    Added placeholder logo

--- components/header.js
+++ components/header.js
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+
+import { Container, Flex } from '../styles/globalStyles';
+import { HeaderContainer } from '../styles/headerStyles';
+
 const Header = () => {
   return (
-    <>
-      Header
-    </>
+    <HeaderContainer>
+      <Container>
+        <Flex spaceBetween flexStart noHeight>
+          <Link href='/'>
+            <img src={require('../assets/img/logo.png')} alt="Logo" />
+          </Link>
+        </Flex>
+      </Container>
+    </HeaderContainer>
   );
 };
 
--- styles/headerStyles.js
+++ styles/headerStyles.js
@@ -1,2 +1,16 @@
 import styled, { css } from "styled-components";
 import { motion } from 'framer-motion';
+
+export const HeaderContainer = styled.header`
+  height: 0px;
+  width: 100%;
+  position: absolute;
+  top: 72px;
+  right: 0;
+  left: 0;
+  z-index: 99;
+
+  img {
+    height: 36px;
+  }
+`;
